ba4a3d2aa0580e525e5aa4a86e11e3754de83662
jordanlevy96 <jordanlevy96@gmail.com>
2023-07-09 13:41:55 +0100

    music and SFX

--- Scripts/Hunter.gd
+++ Scripts/Hunter.gd
@@ -1,10 +1,12 @@
 extends AnimatedSprite2D
 
 @onready var player = get_owner().get_node("Player")
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 func _on_crosshair_shot(_position: Vector2):
 	if not is_playing() and player.player_alive:
 		play()
+		audio_stream_player_2d.play(0.0)
 
 func _on_animation_looped():
 	stop()
